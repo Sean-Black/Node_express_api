8e9809858066172d171be17506dff7a346829fe9
Sean Black <mailseanblack@yahoo.com>
2019-04-23 17:55:06 +0100

    Initial Commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+
+const productRoutes = require("./api/routes/products");
+const orderRoutes = require("./api/routes/orders");
+
+app.use(morgan("dev"));
+
+app.use("/products", productRoutes);
+app.use("/orders", orderRoutes);
+
+app.use((req, res, next) => {
+  const error = new Error("Sorry Not Found");
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message
+    }
+  });
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(3000, () => {
+  console.log(`The app is up and running on port ${port}`);
+});
+
+module.exports = app;
